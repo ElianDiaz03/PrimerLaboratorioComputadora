867ebf77da986083346bf807b7e7c518e6977c70
Wenpineda <cp0428032021@unab.edu.sv>
2021-08-16 15:30:28 -0600

    funciones WCH

--- Computadora/Computadora/Negocio/NClsComputadora.cs
+++ Computadora/Computadora/Negocio/NClsComputadora.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Computadora.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,29 @@ namespace Computadora.Negocio
 {
     class NClsComputadora
     {
+        public String Encender (ClsComputadora computadora)
+        {
+
+
+            return " El monitor de la computadora " + computadora.Monitor + " Con una RAM de " + computadora.RAM1 + " Esta Encendida ";
+
+       
+
+    }
+       public String Apagar (ClsComputadora computadora)
+       {
+
+            return " El mouse de la computadora " + computadora.Mause + " Con el monitor de " + computadora.Monitor + " Estan apagandose ";
+
+            
+       }
+        public String Reiniciar (ClsComputadora computadora)
+        {
+
+
+            return " La RAM de la computadora " + computadora.RAM1 + " Con la placa madre de " + computadora.Placamadre + " Estan Reiniciandose " ; 
+       
+        }
     }
 }
+
